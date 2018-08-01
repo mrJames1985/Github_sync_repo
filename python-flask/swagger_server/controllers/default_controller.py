e5a9881d01fd806e0a2270f79e0ecfdb531094d8
sirJames <Marcus.James@smartbear.com>
2018-08-01 20:42:49 +0000

    Generated code by SwaggerHub

---
+++ python-flask/swagger_server/controllers/default_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from swagger_server.models.customer_type import CustomerType  # noqa: E501
+from swagger_server import util
+
+
+def new_person_post():  # noqa: E501
+    """Create new person
+
+    Create new person # noqa: E501
+
+
+    :rtype: List[CustomerType]
+    """
+    return 'do some magic!'
---
+++ python-flask/swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.customer_type import CustomerType
+from swagger_server.models.person_obj import PersonObj
